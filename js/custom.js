4fac0c9e719d8320c2602a507c5e4764444fdb46
vernepa1 <vernepa1@fit.cvut.cz>
2016-12-07 19:02:52 +0100

    Update custom.js
    Driver details

--- js/custom.js
+++ js/custom.js
@@ -19,6 +19,16 @@ function DiscardDriverButton() {
     
 }
 
+function changeIcon(domImg,srcImage) {
+    var img = new Image();
+    img.onload = function()
+    {
+        // Load completed
+        domImg.src = this.src;
+    };
+    img.src = srcImage;
+}
+
 
 function OKButton() {
     if ($('#FromBox').val().length == 0) {
